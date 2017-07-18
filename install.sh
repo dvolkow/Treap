e955c3f094342567112245a8fee865d67a109421
Daniel Wolkow <volkov12@rambler.ru>
2017-07-18 19:28:47 +0300

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
-sudo mkdir /usr/include/my_dev
-sudo cp ./include/treap.hpp /usr/include/my_dev/
+f=$1
+
+if ! [ -d /usr/include/my_dev ];
+then 
+	sudo mkdir /usr/include/my_dev
+fi
+
+if [ "$f" = "debug" ] 
+then 
+	echo "Установлена дебажная версия <my_dev/treap.hpp>"
+	sudo cp ./debug/treap.hpp /usr/include/my_dev/
+else
+	echo "Установлена релизная версия <my_dev/treap.hpp>"
+	sudo cp ./include/treap.hpp /usr/include/my_dev/
+fi
+
